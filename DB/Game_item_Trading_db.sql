1a21c0cbb2f57677d5c33cca83c5c5e21debd55b
Hoang Hiep <hiepnhhe171696@fpt.edu.vn>
2023-05-29 21:53:40 +0700

    AddDB

---
+++ DB/Game_item_Trading_db.sql
@@ -0,0 +1,92 @@
+create database game_items_trading;
+drop database game_items_trading;
+use game_items_trading;
+CREATE TABLE GameItems(
+	item_id INT not null auto_increment,
+    primary key(item_id),
+	skin_name VARCHAR(50),
+	item_name VARCHAR(50),
+	type VARCHAR(50),
+	rarity VARCHAR(50),
+    img varchar(50)
+);
+CREATE TABLE GameAccount(
+	Game_id INT not null auto_increment,
+    primary key(Game_id),
+	realname VARCHAR(50),
+	username VARCHAR(50),
+	password VARCHAR(50),
+	dob DATE, /*du 18 tuoi*/
+	email VARCHAR(100),
+	gender VARCHAR(10),
+	avatar VARCHAR(50)
+);
+CREATE TABLE UserItem( -- save items in possession of each account --  
+	user_item_id INT not null auto_increment,
+    primary key(user_item_id),
+    game_id int,
+    item_id int,
+	foreign key(game_id) references GameAccount(Game_id),
+	foreign key(item_id) references GameItems(item_id)
+);
+CREATE TABLE Role(
+	role_id INT not null auto_increment,
+    primary key(role_id),
+	role_name VARCHAR(50)
+);
+CREATE TABLE UserAccount(
+	User_id int not null auto_increment,
+    primary key(User_id),
+	username VARCHAR(50),
+	password VARCHAR(50),
+	game_id int,
+    role_id int,
+    money_amount double,
+	foreign key(game_id) references GameAccount(Game_id),
+	foreign key(role_id) references Role(role_id)
+);
+CREATE TABLE Notification (
+    Noti_id INT NOT NULL auto_increment,
+    PRIMARY KEY (Noti_id),
+    date date,
+    User_id INT,
+    FOREIGN KEY (User_id)
+        REFERENCES UserAccount (User_id),
+    noti_content VARCHAR(50),
+    img VARCHAR(50)
+);
+create table MarketItems(-- show item on market-- 
+	id int not null auto_increment,
+    primary key(id),
+    user_id int,
+    foreign key(user_id) references UserAccount(User_id),
+    item_id int,
+    foreign key(item_id) references GameItems(item_id)
+);
+create table TradeItems(
+	id int not null auto_increment,
+    primary key(id),
+    give_id int,
+    foreign key(give_id) references useritem(user_item_id),
+    rec_id int,
+    foreign key(rec_id) references gameitems(item_id)
+);
+create table PaymentRequest ( -- adminscreen --
+	id int not null auto_increment,
+    primary key(id),
+    user_id int,
+    money double,
+    date date,
+    foreign key(user_id) references UserAccount(User_id),
+    img varchar(50)
+);
+create table RetrieveRequest(
+	id int not null auto_increment,
+    primary key (id),
+    user_id int,
+    date date,
+    foreign key(user_id) references UserAccount(User_id),
+    retrieve_amount double,
+    bank_details varchar(50) 
+);
+
