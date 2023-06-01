a9e4cd6607d9cb14518f342f72a98a6b17f30004
Tran The Hung <Asus@LAPTOP-HJKNT011>
2023-06-02 00:00:31 +0700

    Added sendAllUserNotification

--- java_web_projects/In_Game_Items_Tradings/src/java/dao/NotificationDAO.java
+++ java_web_projects/In_Game_Items_Tradings/src/java/dao/NotificationDAO.java
@@ -1,6 +1,6 @@
 /*
 *Programmer: Trần Thế Hùng 
-*Description: This file describes the model of notification that will appear on dashboard
+*Description: This file is a DAO for doing CRUD operations on the Notification table
  */
 package dao;
 
@@ -21,7 +21,7 @@ public class NotificationDAO {
     //Function to get all items in the market 
 
     public static ArrayList<Notification> getAllUserNotification(int user_id) {
-        ArrayList<Notification> notiList = new ArrayList<>();
+        ArrayList<Notification> list = new ArrayList<>();
         Notification notification = null;
         int notificationLimit = 10;
         try {
@@ -42,15 +42,15 @@ public class NotificationDAO {
                     notification.setDate(rs.getString("date"));
                     notification.setNoti_content(rs.getString("noti_content"));
                     notification.setImg(rs.getString("img"));
-                    notiList.add(notification);
+                    list.add(notification);
                 }
                 call.close();
                 con.close();
             }
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println("ERROR IN getAllUserNotification");
         }
-        return notiList;
+        return list;
     }
 
     public static void insertNotification(Notification notification) {
@@ -73,7 +73,36 @@ public class NotificationDAO {
                 con.close();
             }
         } catch (Exception e) {
-            System.out.println("Error");
+            System.out.println("ERROR IN insertNotification");
+        }
+    }
+
+    public static void sendNotificationToAllUser(Notification notification,
+            ArrayList<Integer> userIdList) {
+        try {
+            DBContext db = new DBContext();
+            Connection con = db.getConnection();
+            //if connection is secured, proceed to execute query and retrieve data into and return a list
+            if (con != null) {
+                String sql = "INSERT INTO notification (date, user_id, "
+                        + "noti_content, img) VALUES (?, ?, ?, ?)";
+                PreparedStatement preparedStatement = con.prepareStatement(sql);
+                for (int userId : userIdList) {
+                    System.out.println(userId);
+                    preparedStatement.setString(1, notification.getDate());
+                    preparedStatement.setInt(2, userId);
+                    preparedStatement.setString(3, 
+                            notification.getNoti_content());
+                    preparedStatement.setString(4, notification.getImg());
+                    if (preparedStatement.executeUpdate() != 1) {
+                        System.out.println("ERROR INSERTING ADMIN NOTIFICATION "
+                                + "ID = " + userId);
+                    }
+                }
+                con.close();
+            }
+        } catch (Exception e) {
+            System.out.println("Error in sendNotificationToAllUser");
         }
     }
 //    To be further plan    
@@ -87,7 +116,7 @@ public class NotificationDAO {
 //            System.out.println("Error");
 //        }
 //    }
-    
+
     public static void main(String[] args) {
         NotificationDAO dao = new NotificationDAO();
 //        insertNotification(new Notification (1, "2003-05-01", "bruh", "buy"));
