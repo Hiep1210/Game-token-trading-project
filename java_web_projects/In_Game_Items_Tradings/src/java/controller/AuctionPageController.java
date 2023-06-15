d454360383b8092085d2b8808c95b4487a9af24a
Tran The Hung <>
2023-06-15 17:03:17 +0700

    Fix starting bid amount to highest bid amount

---
+++ java_web_projects/In_Game_Items_Tradings/src/java/controller/AuctionPageController.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import dao.AuctionDAO;
+import dao.BidDAO;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import model.Auction;
+
+/**
+ *
+ * @author Asus
+ */
+@WebServlet(name = "AuctionPageController", urlPatterns = {"/AuctionPageController"})
+public class AuctionPageController extends HttpServlet {
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        ArrayList<Auction> auctionList = AuctionDAO.getAllAuctions();
+        double amount;
+        for (Auction auction : auctionList) { 
+            amount = BidDAO.getHighestBidFromAuctionId(auction.getAuctionId());
+            //If atleast 1 bid was found for auction set lowest bid to that bid amount
+            if (amount > 0) {
+                auction.setLowestBid(amount);
+            }
+        }
+        request.setAttribute("auctionList", auctionList);
+        request.getRequestDispatcher("auction.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
