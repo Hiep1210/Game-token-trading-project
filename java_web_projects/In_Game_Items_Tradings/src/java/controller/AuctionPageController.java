ce42298f78ebf823ce4dc12925cfba3e9f6a144f
Tran The Hung <>
2023-06-16 18:01:29 +0700

    Delete unnecessary comments and refractor code

--- java_web_projects/In_Game_Items_Tradings/src/java/controller/AuctionPageController.java
+++ java_web_projects/In_Game_Items_Tradings/src/java/controller/AuctionPageController.java
@@ -22,18 +22,18 @@ import model.Auction;
 @WebServlet(name = "AuctionPageController", urlPatterns = {"/AuctionPageController"})
 public class AuctionPageController extends HttpServlet {
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        goToAuctionPage(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        goToAuctionPage(request, response);
+    }
+
+    public void goToAuctionPage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ArrayList<Auction> auctionList = AuctionDAO.getAllAuctions();
         double amount;
         for (Auction auction : auctionList) {
@@ -46,28 +46,5 @@ public class AuctionPageController extends HttpServlet {
         request.setAttribute("auctionList", auctionList);
         request.getRequestDispatcher("auction.jsp").forward(request, response);
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
