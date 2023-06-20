a11586de3266323ffad49293fdbd2b5070b253cf
SaltyPotatOwO <potatoking447@gmail.com>
2023-06-20 21:38:38 +0700

    - Added deleteCartWithMarketItem and deleteCartItem

--- java_web_projects/In_Game_Items_Tradings/src/java/dao/CartDAO.java
+++ java_web_projects/In_Game_Items_Tradings/src/java/dao/CartDAO.java
@@ -18,10 +18,16 @@ import model.Cart;
  * @author Inspiron
  */
 public class CartDAO {
+
     private static final String SELECTITEMS = "SELECT c.id,c.buyer_id,m.id, m.game_account_name, m.user_id, "
             + "m.price, m.begin_date, m.end_date,g.* FROM cart c, marketitems m, gameitems g "
             + "where c.market_items_id = m.id and m.item_id = g.id and c.buyer_id = ?";
-    public static ArrayList<Cart> getAllCartItems(int buyerid){
+    
+    private static final String SELECTONEITEM = "SELECT c.id,c.buyer_id,m.id, m.game_account_name, m.user_id, "
+            + "m.price, m.begin_date, m.end_date,g.* FROM cart c, marketitems m, gameitems g "
+            + "where c.market_items_id = m.id and m.item_id = g.id and c.id = ?";
+    
+    public static ArrayList<Cart> getAllCartItems(int buyerid) {
         ArrayList<Cart> list = new ArrayList<>();
         Cart items = null;
         Connection con = null;
@@ -38,22 +44,23 @@ public class CartDAO {
                 //run a loop to save queries into model
                 while (rs.next()) {
                     items = new Cart(rs.getInt(1), rs.getInt(2), rs.getInt(3), rs.getString(4), rs.getInt(5), rs.getDouble(6),
-                            rs.getString(7), rs.getString(8), rs.getInt(9), rs.getString(10), rs.getString(11), rs.getString(12), 
+                            rs.getString(7), rs.getString(8), rs.getInt(9), rs.getString(10), rs.getString(11), rs.getString(12),
                             rs.getString(13), rs.getString(14), rs.getString(15));
                     list.add(items);
                 }
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
-        } 
-        try{
+        }
+        try {
             statement.close();
             con.close();
-        }catch(SQLException s){
+        } catch (SQLException s) {
         }
         return list;
     }
-    public static boolean insertCartItem(int buyerid, int marketid ){
+
+    public static boolean insertCartItem(int buyerid, int marketid) {
         try {
             DBContext db = new DBContext();
             Connection con = db.getConnection();
@@ -61,7 +68,9 @@ public class CartDAO {
             PreparedStatement statement = con.prepareStatement(sql);
             statement.setInt(1, buyerid);
             statement.setInt(2, marketid);
-           if(statement.executeUpdate() < 1) throw new Exception();
+            if (statement.executeUpdate() < 1) {
+                throw new Exception();
+            }
             con.close();
             statement.close();
             return true;
@@ -70,24 +79,48 @@ public class CartDAO {
         }
         return false;
     }
-    public static boolean deleteCartItem(int id){
+    
+    public static boolean deleteCartWithMarketItemId(int marketetItemId) {
+        boolean deleteStatus = true;
         try {
-                    DBContext db = new DBContext();
+            DBContext db = new DBContext();
             Connection con = db.getConnection();
-            String sql = "delete from cart where id = "+id;
+            String sql = "delete from cart where market_items_id = " + marketetItemId;
             Statement statement = con.createStatement();
-            if(statement.executeUpdate(sql) < 1) throw new Exception();
+            if (statement.executeUpdate(sql) < 1) {
+                deleteStatus = false;
+                throw new Exception();
+            }
             con.close();
             statement.close();
             return true;
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        return false;
+        return deleteStatus;
     }
+    
+    public static boolean deleteCartItem(int id) {
+        boolean deleteStatus = true;
+        try {
+            DBContext db = new DBContext();
+            Connection con = db.getConnection();
+            String sql = "delete from cart where id = " + id;
+            Statement statement = con.createStatement();
+            if (statement.executeUpdate(sql) < 1) {
+                deleteStatus = false;
+                throw new Exception();
+            }
+            con.close();
+            statement.close();
+            return true;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return deleteStatus;
+    }
+
     public static void main(String[] args) throws SQLException {
-        getAllCartItems(1);
-        insertCartItem(2, 3);
-        deleteCartItem(21);
+//        System.out.println(getCartFromId(1));
     }
 }
