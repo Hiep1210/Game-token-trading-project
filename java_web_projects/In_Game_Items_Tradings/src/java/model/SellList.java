6797fe8dc6a0205bd2a0d28d3f0732b84f3d8cde
lunasimp <133180054+lunasimp@users.noreply.github.com>
2023-06-20 10:31:45 +0700

    Add files via upload
    Add SellList

---
+++ java_web_projects/In_Game_Items_Tradings/src/java/model/SellList.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.List;
+
+/**
+ *
+ * @author ksiks
+ */
+public class SellList extends MarketItems{
+    int id;
+    List<MarketItems> items ;
+    
+    public SellList(int id, int buyer_id, int mid, String game, int user_id, double price, String begindate, String enddate, int gid, String skin_name, String item_name, String type, String rarity, String exterior, String img) {
+        super(mid, game, user_id, price, begindate, enddate, gid, skin_name, item_name, type, rarity, exterior, img);
+        this.id = id;
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    
+
+    MarketItems getItemById(int id) {
+        for (MarketItems item : items) {
+            if ( item.getId() == id) {
+                return item;
+            }
+        }
+        return null;
+    }
+    
+    public void removeItem(int id) {
+        if (getItemById(id) != null) {
+            items.remove(getItemById(id));
+        }
+    }
+}
