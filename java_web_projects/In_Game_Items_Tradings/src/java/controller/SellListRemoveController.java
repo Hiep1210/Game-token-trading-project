67d8aa207d5e2fb77aba4ee4a23c10ad2207d56d
lunasimp <133180054+lunasimp@users.noreply.github.com>
2023-06-21 15:53:37 +0700

    Update SellListRemoveController.java

--- java_web_projects/In_Game_Items_Tradings/src/java/controller/SellListRemoveController.java
+++ java_web_projects/In_Game_Items_Tradings/src/java/controller/SellListRemoveController.java
@@ -40,21 +40,21 @@ public class SellListRemoveController {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-                ArrayList<GameItemsDAO> sellList = new ArrayList<>();
-        try {
-            HttpSession session = request.getSession();
-            Object object = session.getAttribute("sellList");
-            int cartId = Integer.parseInt(request.getParameter("id"));
-//            SellList cart = (SellList) object;
-//            cart.removeItem(cartId);
-            request.setAttribute("sellList", sellList);
-            request.getRequestDispatcher("sell.jsp").forward(request, response);
-        } catch (Exception e) {
-            response.getWriter().println(e);
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet DeleteCartController</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet DeleteCartController at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
         }
-    }
+    } 
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
