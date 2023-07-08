a759f237304a7eaca3c58ad7a7376c0e350d1b37
IAmKou <luongcu111@gmail.com>
2023-07-08 17:56:32 +0700

    update code

--- java_web_projects/In_Game_Items_Tradings/src/java/model/Thread.java
+++ java_web_projects/In_Game_Items_Tradings/src/java/model/Thread.java
@@ -3,7 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package model;
-
 /**
  *
  * @author ACER
@@ -12,60 +11,71 @@ public class Thread {
    private int id;
    private String ttitle;
    private String tcontent;
-   private String ticon;
    private String ttag;
+   private String tauthor;
+   private int user_id;
    
    public Thread(){
        
    }
 
-    public Thread(int id, String ttitle, String tcontent, String ticon, String ttag) {
-        this.id = id;
-        this.ttitle = ttitle;
-        this.tcontent = tcontent;
-        this.ticon = ticon;
-        this.ttag = ttag;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getTcontent() {
-        return tcontent;
-    }
-
-    public String getTicon() {
-        return ticon;
-    }
-
-    public String getTtag() {
-        return ttag;
-    }
-
-    public String getTtitle() {
-        return ttitle;
-    }
-
     public void setId(int id) {
         this.id = id;
     }
 
-    public void setTcontent(String tcontent) {
-        this.tcontent = tcontent;
-    }
-
-    public void setTicon(String ticon) {
-        this.ticon = ticon;
+    public void setTtitle(String ttitle) {
+        this.ttitle = ttitle;
     }
 
     public void setTtag(String ttag) {
         this.ttag = ttag;
     }
 
-    public void setTtitle(String ttitle) {
-        this.ttitle = ttitle;
+    public void setTcontent(String tcontent) {
+        this.tcontent = tcontent;
     }
-   
-}
 
+    public void setTauthor(String tauthor) {
+        this.tauthor = tauthor;
+    }
+
+    public String getTtitle() {
+        return ttitle;
+    }
+
+    public String getTcontent() {
+        return tcontent;
+    }
+
+    public String getTtag() {
+        return ttag;
+    }
+
+    public String getTauthor() {
+        return tauthor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public Thread(int id, String ttitle, String tcontent, String ttag, String tauthor, int user_id) {
+        this.id = id;
+        this.ttitle = ttitle;
+        this.tcontent = tcontent;
+        this.ttag = ttag;
+        this.tauthor = tauthor;
+        this.user_id = user_id;
+    }
+
+  
+
+}
