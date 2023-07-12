878b5d7c9139f5cac284dab353c3da2249f72a9e
laamwwibu <lampphe172382@fpt.edu.vn>
2023-07-12 23:38:28 +0700

    merge commit

---
+++ java_web_projects/In_Game_Items_Tradings/src/java/controller/SellNotificationController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import dao.NotificationDAO;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import model.GameItems;
+import model.Notification;
+import model.User;
+
+@WebServlet(name = "SellNotificationController", urlPatterns = {"/SellNotificationController"})
+public class SellNotificationController extends HttpServlet {
+
+    public void sellNotification(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            User user = (User) request.getSession().getAttribute("user");
+            GameItems gameItems = (GameItems) request.getAttribute("gameItems");
+            String redirect = "BuyPageController";// if redirect attribute is null auto redirect to BuyPageController
+            String notiContent;
+            Notification notification;
+            if (user == null) { // if session does not contain any user instance
+                redirect = "BuyPageController";
+            } else { // if the user sell the item correctly
+                notiContent = "You have sold " + gameItems.getSkinName() + " " + gameItems.getItemName() + "successfully";
+                notification = new Notification(user.getId(), LocalDateTime.now(), notiContent, "sell");
+                NotificationDAO.insertNotification(notification);
+            }
+            request.getRequestDispatcher(redirect).forward(request, response);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
